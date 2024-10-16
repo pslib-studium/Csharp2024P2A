28f284bb92bbee156df37165a8cbbdad9f82b629
MichalStehlik <michal.stehlik@pslib.cz>
2024-10-16 10:46:28 +0200

    class

---
+++ c05class/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+using c05class;
+
+Student alf = new Student("Petr","Petrov");
+//Console.WriteLine(alf.firstName);
+Console.WriteLine(alf.GetFirstName());
+//alf.firstName = "Alfa";
+alf.SetFirstName("Elon");
+Console.WriteLine(alf.GetFirstName());
+alf.SetFirstName("Alfa");
+Console.WriteLine(alf.GetFullName());
+Student bruce = new Student();
+Console.WriteLine(bruce.FirstName);
---
+++ c05class/Student.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace c05class
+{
+    // zapoudření - encapsulation
+    class Student
+    {
+        private string _firstName = "Alfons"; // datový člen, attribute, field
+        private string _lastName = "Vopička"; // private members starts with _
+        private string something = default;
+
+        // property
+        public string FirstName
+        {
+            get { return _firstName; }
+            set { if (value != "Elon") _firstName = value; }
+        }
+
+        // konstruktor = constructor
+        public Student(string firstName, string lastName)
+        {
+            //this.firstName = firstName; // this = instance of this class
+            //this.lastName = lastName;
+            _firstName = firstName;
+            _lastName = lastName;
+        }
+        // bezparametrický konstruktor
+        // přetížené konstrukrory - overloaded constructors
+        public Student()
+        {
+            _firstName = "Bruce";
+            _lastName = "Wayne";
+        }
+
+        public string GetFirstName()
+        {
+            return _firstName;
+        }
+        public string GetLastName() {
+            return _lastName;
+        }
+        public void SetFirstName(string newFirstName)
+        {
+            if (newFirstName != "Elon") _firstName = newFirstName;
+        }
+        public void SetLastName(string newLastName)
+        {
+            if (newLastName != "Musk") _lastName = newLastName;
+        }
+        public string GetFullName()
+        {
+            return _firstName + " " + _lastName;
+        }
+    }
+}
