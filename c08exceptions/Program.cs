4caa794d6b3cb835f691afc6d4fbed4037c33e34
Michal Stehlík <michal.stehlik@pslib.cz>
2024-11-13 13:26:23 +0100

    exception + static

---
+++ c08exceptions/Program.cs
@@ -0,0 +1,40 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+float a = 7;
+float b = 0;
+try 
+{
+    float c = Divide(a, b); // <--- tady to může hodit výjimku
+    Console.WriteLine($"a/b = {c}");
+}
+catch (DivideByZeroException e)
+{
+    Console.WriteLine("CHYBA dělení: " + e.Message);
+}
+catch (Exception e)
+{
+    Console.WriteLine("Divná chyba: " + e.Message);
+}
+finally {
+    Console.WriteLine("Konec programu.");
+}
+
+float Divide(float a, float b)
+{
+    if (a == 7)
+    {
+        throw new MySpecialException("Nesmíš dělit sedmičku.");
+    }
+    if (b == 0)     {
+        throw new DivideByZeroException("Seš idiot, dělíš nulou.");
+    }
+    return a / b;
+}
+
+class MySpecialException : Exception
+{
+    public MySpecialException(string message) : base(message)
+    {
+    }
+}
---
+++ c09static/Program.cs
@@ -0,0 +1,42 @@
+﻿// See https://aka.ms/new-console-template for more information
+Human h = new Human("Pepa", 16); // instance třídy Human
+h.SayHello();
+Human h2 = new Human("Jirka", 18); // instance třídy Human
+h2.SayHello();
+Human.SayHelloStatic();
+
+Console.WriteLine(MathOps.Divide(2,3));
+class Human
+{
+    public string Name { get; set; }
+    public int Age { get; set; }
+    static int Counter { get; set; } = 0;
+    public Human(string name, int age)
+    {
+        Name = name;
+        Age = age;
+        Counter++;
+    }
+    public void SayHello()
+    {
+        Console.WriteLine($"Ahoj, jsem {Name} a je mi {Age} let.");
+        Console.WriteLine($"Je tu {Counter} lidí.");
+    }
+
+    public static void SayHelloStatic()
+    {
+        Console.WriteLine($"Ahoj, je tu {Counter} lidí.");
+    }
+}
+
+static class MathOps
+{
+    public static float Divide(float a, float b)
+    {
+        if (b == 0)
+        {
+            throw new DivideByZeroException("Seš idiot, dělíš nulou.");
+        }
+        return a / b;
+    }
+}
